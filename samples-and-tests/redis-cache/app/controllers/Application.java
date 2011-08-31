73aeefb32ec65cf11eb0209661297bf18d50d6ee
tkral <tkral@salesforce.com>
2011-08-31 13:21:35 -0700

    Updated javascript for cache monitor

--- samples-and-tests/redis-cache/app/controllers/Application.java
+++ samples-and-tests/redis-cache/app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.cache.Cache;
+import play.libs.Time;
 import play.mvc.Controller;
 
 public class Application extends Controller {
@@ -9,18 +10,33 @@ public class Application extends Controller {
     	render();
     }
 
-//    public static void add(String key, String value) {
-//    	Cache.add(key, value);
-//    	index();
-//    }
-//    
-    public static void set(String key, String value) {
-    	Cache.set(key, value);
+    public static void set(String key, String value, String expiration) {
+    	
+    	try {
+    		if (expiration == null || "".equals(expiration)) {
+    			Cache.set(key, value);
+    		} else {
+    			Cache.set(key, value, expiration);
+    		}
+    	} catch (IllegalArgumentException e) {
+    		flash.error("Illegal expiration value %s (e.g. 10s, 1mn, 3h)", expiration);
+    	}
+    	
     	index();
     }
-//    
-//    public static void replace(String key, String value) {
-//    	Cache.replace(key, value);
-//    	index();
-//    }
+    
+    public static void replace(String key, String value, String expiration) {
+    	
+    	try {
+    		if (expiration == null || "".equals(expiration)) {
+    			Cache.replace(key, value);
+    		} else {
+    			Cache.replace(key, value, expiration);
+    		}
+    	} catch (IllegalArgumentException e) {
+    		flash.error("Illegal expiration value %s (e.g. 10s, 1mn, 3h)", expiration);
+    	}
+    	
+    	index();
+    }
 }
